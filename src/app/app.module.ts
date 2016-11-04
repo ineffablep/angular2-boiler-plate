437dec3cd55244b97b7f093c16744c65770c6118
Venkata P <venksaty@intranet.barcapint.com>
2016-11-04 17:01:33 +0000

    less

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/shared/navigation/navigation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'sui-nav',
+  templateUrl: './navigation.component.html'
+})
+export class NavigationComponent {
+
+}
